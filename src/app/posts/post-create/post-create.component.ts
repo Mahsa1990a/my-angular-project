a9ceafb6109130347cf24f7c7d33eb5ebf1db615
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-09-16 09:49:02 -0400

    added ngModel for writing and reading and update the code based on ngModel

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -9,13 +9,15 @@ export class PostCreateComponent { // we turn typescript class to component angu
 
   // property or variable in class(define it without let or const):
   newPost = 'NO CONTENT';
+  enteredValue: '';
 
-  onAddPost(postInput: HTMLTextAreaElement) { // add this method to pass it as a click event in html
-    // alert("Post added!");
+  // onAddPost(postInput: HTMLTextAreaElement) { // add this method to pass it as a click event in html
+  onAddPost() { // add this method to pass it as a click event in html
 
-    console.log("postInput: ", postInput); //<textarea rows="6"></textarea>
-    console.dir("postInput: ", postInput); //will get js object propetries
+    // console.log("postInput: ", postInput); //<textarea rows="6"></textarea>
+    // console.dir("postInput: ", postInput); //will get js object propetries
     // this.newPost = "The user's post"    UPDATE to acceess to the value we're typing in textarea
-    this.newPost = postInput.value;
+    // this.newPost = postInput.value;
+    this.newPost = this.enteredValue;
   }
 }
