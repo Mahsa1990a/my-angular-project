7c558c0c571fa2f0a2b9588923c858276486374b
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-09-16 10:54:46 -0400

    import matInputModule and use it inside html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
+// angular imports:
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
+// import { MatButtonModule } from "@angular/material/button";
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     FormsModule,//with adding this we're unlocking some form's feature such as ngModel
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
