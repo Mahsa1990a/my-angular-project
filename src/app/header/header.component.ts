8f320ba87cb87e20aaf9da03055bb8bee913088f
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-09-17 09:56:07 -0400

    Adding a Toolbar

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'; // Component decorator
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+
+export class HeaderComponent { // we turn typescript class to component angular understand by adding @Component
+
+}
