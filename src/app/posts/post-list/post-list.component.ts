c0822421a14dba4fac6fec6faf56e9ecb3f52fec
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-09-17 10:20:23 -0400

    Creating post-list for Outputting Posts

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core'; // Component decorator
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent {
+  posts = [
+    { title: 'First Post', content: 'This is a POST1' },
+    { title: 'Second Post', content: 'This is a POST2' },
+    { title: 'Third Post', content: 'This is a POST3' }
+  ]
+}
